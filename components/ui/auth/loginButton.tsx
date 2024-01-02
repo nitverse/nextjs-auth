8ec4d7450f641a630268c6624fa7ad0e0634c0c0
nitverse <nmakwana@gmail.com>
2024-01-02 23:49:42 +0530

    Added Ui for Home Page

---
+++ components/ui/auth/loginButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+const LoginButton: React.FC<LoginButtonProps> = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/auth/login");
+  };
+
+  if (mode === "modal") {
+    return <span>TODO: Implement modal</span>;
+  }
+
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
+
+export default LoginButton;

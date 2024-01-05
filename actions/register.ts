19394a3a7ea165ed5aca9d850a1d81bdeac27d2d
Nitant Makwana <nmakwana496@gmail.com>
2024-01-05 17:50:00 +0530

    setup neondb with prisma and register user in db

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from 'bcrypt'
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values : z.infer<typeof RegisterSchema>) => {
     // console.log(values);
@@ -9,5 +12,24 @@ export const register = async (values : z.infer<typeof RegisterSchema>) => {
     if(!validatedFields.success){
         return {error : "Invalid fields!"};
     }
-    return {success : "Email Sent! "};
+
+    const { email , password , name} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password , 10);
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser) {
+        return {error : "Email Already In Use"}
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword,
+        },
+    })
+
+    //Todo : Send user verification token email
+
+    return {success : "User Created! "};
 }
